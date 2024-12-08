fe82ad7d03682cf44aec0793f579b584221ba491
Prateek Giridhar <pgiridha@Prateeks-MacBook-Pro-3.local>
2024-12-08 14:54:33 +0530

    Updated config of the tool

--- ImageAnalyzer/config/constants.py
+++ ImageAnalyzer/config/constants.py
@@ -1,3 +1,4 @@
-# Define constants used across the application
+# Constants used across the application
 
 DEFAULT_IMAGE_FORMATS = ['png', 'bmp']  # Lossless formats preferred for steganography
+MAX_MESSAGE_LENGTH = 1024  # Maximum length of the message to encode
