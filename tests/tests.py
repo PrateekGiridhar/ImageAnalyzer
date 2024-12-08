53613adeb11f0c87efd91e0330241177f7e0760f
Prateek Giridhar <pgiridha@Prateeks-MacBook-Pro-3.local>
2024-12-08 11:54:27 +0530

    Created test file for LSB

---
+++ tests/tests.py
@@ -0,0 +1,17 @@
+import unittest
+from stegsolve.core.lsb import encode_lsb, decode_lsb
+
+class TestLSB(unittest.TestCase):
+    
+    def test_encode_decode(self):
+        input_image = "data/sample_image.png"
+        output_image = "data/output_image.png"
+        test_message = "Hello World!"
+        
+        encode_lsb(input_image, output_image, test_message)
+        decoded_message = decode_lsb(output_image)
+        
+        self.assertEqual(test_message, decoded_message)
+
+if __name__ == "__main__":
+    unittest.main()
